f30bd5c96281b804c0c4eaecb42df55ad27f5abd
marufameem <heymarufa@gmail.com>
2024-05-29 03:10:59 +0600

    modify previous ui test and register ui

--- app/src/androidTest/java/com/example/onser/PaymentActivityTest.java
+++ app/src/androidTest/java/com/example/onser/PaymentActivityTest.java
@@ -1,17 +1,16 @@
 package com.example.onser;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.filters.LargeTest;
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 @LargeTest
